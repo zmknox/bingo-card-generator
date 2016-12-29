b0f60cdae6d8cf2d60cd63bba7a59d5742494b70
Zach Knox <zmknox@gmail.com>
2016-12-29 11:39:08 -0500

    Barebones HTML page looks good

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/', function(req, res){
 
 app.use(express.static('public'));
 
+app.get('/css/bootstrap.min.css', function(req, res){
+  res.sendfile('node_modules/bootstrap/dist/css/bootstrap.min.css');
+});
+
 http.listen(app.get('port'), function(){
     console.log('listening on port', app.get('port'));
 });
